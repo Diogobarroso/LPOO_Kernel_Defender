ca1924e6c9cf44c03385d4c63c70b540e102dc45
zephiro309 <zephiro309@gmail.com>
2014-05-08 00:46:58 +0100

    Made temporary ship follow mouse.

--- Project/core/src/com/mygdx/game/KernelDefender.java
+++ Project/core/src/com/mygdx/game/KernelDefender.java
@@ -6,6 +6,7 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.input.*;
 
 public class KernelDefender extends ApplicationAdapter {
 	SpriteBatch batch;
@@ -19,7 +20,6 @@ public class KernelDefender extends ApplicationAdapter {
 		batch = new SpriteBatch();
         playerTex = new Texture("tempship.png");
         player = new Sprite(playerTex);
-        player.setPosition(640 - player.getWidth() / 2, 360 - player.getHeight() / 2);
         player.setScale(2.0f);
         player.setOrigin(player.getWidth() / 2, player.getHeight() / 2);
         player.setRotation(rotation);
@@ -29,10 +29,31 @@ public class KernelDefender extends ApplicationAdapter {
 	public void render () {
         rotation += rotSpeed * Gdx.graphics.getDeltaTime() % 360.0f;
         player.setRotation(rotation);
+        player.setPosition(Gdx.input.getX() - player.getWidth() / 2, 720 - Gdx.input.getY() - player.getHeight() / 2);
 		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         batch.begin();
             player.draw(batch);
         batch.end();
 	}
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
 }
